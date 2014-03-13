8cc9f1eb15be0fe7ffc772bfa499a549d2fdcaf5
Pierre Molinaro <pierre@pcmolinaro.name>
2014-03-13 19:53:39 +0000

    MAJ suite MAJ GALGAS

--- galgas_sources/GALGAS_OUTPUT/libpm/bdd/C_BDD.cpp
+++ galgas_sources/GALGAS_OUTPUT/libpm/bdd/C_BDD.cpp
@@ -2,7 +2,7 @@
 //                                                                             *
 //     BDD package (implementation of ROBDD)                                 *
 //                                                                             *
-//  This file is part of libpm library                                       *
+//  This file is part of libpm library                                         *
 //                                                                             *
 //  Copyright (C) 1999, ..., 2012 Pierre Molinaro.                             *
 //                                                                             *
@@ -11,7 +11,7 @@
 //  ECN, École Centrale de Nantes (France)                                     *
 //                                                                             *
 //  This library is free software; you can redistribute it and/or modify it    *
-//  under the terms of the GNU Lesser General Public License as published    *
+//  under the terms of the GNU Lesser General Public License as published      *
 //  by the Free Software Foundation; either version 2 of the License, or       *
 //  (at your option) any later version.                                        *
 //                                                                             *
@@ -1847,10 +1847,9 @@ void C_BDD::printBDD (AC_OutputStream & inStream,
 
 //---------------------------------------------------------------------*
 
-static void
-printBDDline (const TC_UniqueArray <char> & inDisplayString,
-              const TC_UniqueArray <PMSInt32> & inNameLengthArray,
-              const PMSInt32 inLeadingSpacesCount) {
+static void printBDDline (const TC_UniqueArray <char> & inDisplayString,
+                          const TC_UniqueArray <PMSInt32> & inNameLengthArray,
+                          const PMSInt32 inLeadingSpacesCount) {
   for (PMSInt32 i=0 ; i<inLeadingSpacesCount ; i++) {
     co << " " ;
   }
