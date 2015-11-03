6565762c9a3444b57d915bb7e403087c080f647e
pm <pm@3c3e2c68-dced-0310-956d-eac7787d6712>
2015-11-03 10:16:50 +0000

    MAJ suite MAJ GALGAS

--- hand_coded_sources/independant_resources_core.cpp
+++ hand_coded_sources/independant_resources_core.cpp
@@ -23,8 +23,6 @@
 #include "command_line_interface/F_Analyze_CLI_Options.h"
 #include "command_line_interface/F_mainForLIBPM.h"
 #include "time/C_Timer.h"
-#include "utilities/TF_sup.h"
-#include "utilities/TF_inf.h"
 
 #include "C_activitiesToSchedule.h"
 #include "activities_core.h"
