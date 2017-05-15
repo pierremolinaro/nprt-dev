f9143ec04f624aa3c2693377681fc46a38126c8f
pm <pm@3c3e2c68-dced-0310-956d-eac7787d6712>
2017-05-15 12:31:51 +0000

    MAJ suite MAJ GALGAS

--- makefile-unix/build.py
+++ makefile-unix/build.py
@@ -27,6 +27,6 @@ LIBPM_DIRECTORY_PATH = dictionary ["LIBPM_DIRECTORY_PATH"]
 sys.path.append (os.path.abspath (LIBPM_DIRECTORY_PATH + "/python-makefiles"))
 from unix_gcc_tools import buildForUnix
 #----------------------------------------------------------------- Build
-buildForUnix (dictionary, "nprt", goal, maxParallelJobs, maxParallelJobs == 1)
+buildForUnix (dictionary, jsonFilePath, "nprt", goal, maxParallelJobs, maxParallelJobs == 1)
 
 #----------------------------------------------------------------------------------------------------------------------*
