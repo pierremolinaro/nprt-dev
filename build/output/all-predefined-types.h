47b3d79b8d86f79c5316d8003ded75de96fa5651
pm <pm@3c3e2c68-dced-0310-956d-eac7787d6712>
2017-11-15 21:59:47 +0000

    MAJ suite MAJ GALGAS

--- build/output/all-predefined-types.h
+++ build/output/all-predefined-types.h
@@ -207,6 +207,8 @@ class GALGAS_uint : public AC_GALGAS_root {
 //--------------------------------- Class Methods
 
 //--------------------------------- Getters
+  public : VIRTUAL_IN_DEBUG class GALGAS_string getter_alphaString (LOCATION_ARGS) const ;
+
   public : VIRTUAL_IN_DEBUG class GALGAS_bigint getter_bigint (LOCATION_ARGS) const ;
 
   public : VIRTUAL_IN_DEBUG class GALGAS_bool getter_canAdd (const class GALGAS_uint & constinOperand0
@@ -2981,6 +2983,8 @@ class GALGAS_uint_36__34_ : public AC_GALGAS_root
 //--------------------------------- Class Methods
 
 //--------------------------------- Getters
+  public : VIRTUAL_IN_DEBUG class GALGAS_string getter_alphaString (LOCATION_ARGS) const ;
+
   public : VIRTUAL_IN_DEBUG class GALGAS_bigint getter_bigint (LOCATION_ARGS) const ;
 
   public : VIRTUAL_IN_DEBUG class GALGAS_bool getter_canAdd (const class GALGAS_uint_36__34_ & constinOperand0
