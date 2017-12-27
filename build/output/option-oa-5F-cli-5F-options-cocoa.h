531c2f350b6aea6880815fb7fc135f81bd209d57
Pierre Molinaro <pierre@pcmolinaro.name>
2017-12-27 17:53:05 +0000

    MAJ suite MAJ GALGAS

--- build/output/option-oa-5F-cli-5F-options-cocoa.h
+++ build/output/option-oa-5F-cli-5F-options-cocoa.h
@@ -19,9 +19,9 @@
 //---------------------------------------------------------------------------------------------------------------------*
 
 void enterOptionsFor_oa_5F_cli_5F_options (NSMutableArray * ioBoolOptionArray,
-                                                       NSMutableArray * ioUIntOptionArray,
-                                                       NSMutableArray * ioStringOptionArray,
-                                                       NSMutableArray * ioStringListOptionArray) ;
+                                           NSMutableArray * ioUIntOptionArray,
+                                           NSMutableArray * ioStringOptionArray,
+                                           NSMutableArray * ioStringListOptionArray) ;
 
 //---------------------------------------------------------------------------------------------------------------------*
 
