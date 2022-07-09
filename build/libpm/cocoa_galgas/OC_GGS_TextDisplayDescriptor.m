4fb0bd99e06633be8fdbaf813c37a7df3d1ce162
pierremolinaro <pierre@pcmolinaro.name>
2022-07-09 20:17:40 +0200

    MAJ suite MAJ GALGAS

--- build/libpm/cocoa_galgas/OC_GGS_TextDisplayDescriptor.m
+++ build/libpm/cocoa_galgas/OC_GGS_TextDisplayDescriptor.m
@@ -106,10 +106,8 @@ static inline NSUInteger imax (const NSUInteger a, const NSUInteger b) { return 
     mTextView.grammarCheckingEnabled = NO ;
     mTextView.continuousSpellCheckingEnabled = NO ;
     [mTextView useAllLigatures: nil] ;
-//    mTextView.allowedInputSourceLocales = [NSArray arrayWithObject: NSAllRomanInputSourcesLocaleIdentifier] ;
- //   mTextView.alignment = NSNaturalTextAlignment ;
     mTextView.automaticQuoteSubstitutionEnabled = NO ;
-    mTextView.smartInsertDeleteEnabled = YES ;
+    mTextView.smartInsertDeleteEnabled = NO ;
     mTextView.automaticDashSubstitutionEnabled = NO ;
     mTextView.layoutManager.allowsNonContiguousLayout = YES ;
     mTextView.layoutManager.usesFontLeading = YES ;
@@ -119,20 +117,8 @@ static inline NSUInteger imax (const NSUInteger a, const NSUInteger b) { return 
     mTextView.allowsImageEditing = YES ;
     mTextView.layoutManager.usesScreenFonts = NO ;
     mTextView.layoutManager.showsControlCharacters = YES ;
-//    mTextView.font = nil ; // [NSFont systemFontOfSize: 0.0] ;
-//    [mTextView.layoutManager setTypesetterBehavior: NSTypesetterLatestBehavior] ;
-  //---
     mTextView.automaticTextReplacementEnabled = NO ;
-//    if ([mTextView respondsToSelector:@selector(setAutomaticTextReplacementEnabled:)]) {
-//      [mTextView setValue:[NSNumber numberWithBool:NO] forKey:@"automaticTextReplacementEnabled"] ;
-//    }
-  //---
-    mTextView.usesFindPanel = YES ;
-//    if ([mTextView respondsToSelector:@selector (setUsesFindBar:)]) {
-//      [mTextView setValue:[NSNumber numberWithBool:YES] forKey:@"usesFindBar"] ;
-//    }else{
-//      mTextView.usesFindPanel = YES ;
-//    }
+    mTextView.usesFindBar = YES ;
   //---
     [mTextView setDelegate: self] ;
   //---
@@ -142,10 +128,6 @@ static inline NSUInteger imax (const NSUInteger a, const NSUInteger b) { return 
     mScrollView.borderType = NSBezelBorder ;
   //--- Vertical Ruler
     mRulerView = [OC_GGS_RulerViewForTextView new] ;
-//    mRulerView = [[OC_GGS_RulerViewForTextView alloc]
-//      initWithScrollView: mScrollView
-//      orientation: NSVerticalRuler
-//    ] ;
     [mScrollView setVerticalRulerView: mRulerView] ;
     [mScrollView.verticalRulerView setRuleThickness: 50.0] ;
     [mScrollView setHasVerticalRuler:YES] ;
@@ -173,14 +155,14 @@ static inline NSUInteger imax (const NSUInteger a, const NSUInteger b) { return 
       context:NULL
     ] ;
     [self refreshShowInvisibleCharacters] ;
-    [documentData.textSyntaxColoring addDisplayDescriptor:self] ;
+    [documentData.textSyntaxColoring addDisplayDescriptor: self] ;
    //--- Set selection
     [[NSRunLoop mainRunLoop]
       performSelector: @selector (setSelectionAndScrollToVisibleAfterInit)
-      target:self
-      argument:nil
-      order:0
-      modes:[NSArray arrayWithObject:NSDefaultRunLoopMode]
+      target: self
+      argument: nil
+      order: 0
+      modes: [NSArray arrayWithObject: NSDefaultRunLoopMode]
     ] ;
   }
   #ifdef DEBUG_MESSAGES
@@ -270,7 +252,7 @@ static inline NSUInteger imax (const NSUInteger a, const NSUInteger b) { return 
   #ifdef DEBUG_MESSAGES
     NSLog (@"%s", __PRETTY_FUNCTION__) ;
   #endif
-  if ([inKeyPath isEqualToString:@"values.PMShowInvisibleCharacters"]) {
+  if ([inKeyPath isEqualToString: @"values.PMShowInvisibleCharacters"]) {
     [self refreshShowInvisibleCharacters] ;
   }else{
     [super
